38dcaf18a4f933809fd7621582a21f75cf193f6d
Steve Bonds <sbonds@gmail.com>
2020-01-29 12:51:00 -0800

    Checking write permission on root-owned files is a wasted effort.
    Root can write them anyhow.

--- controls/os_spec.rb
+++ controls/os_spec.rb
@@ -141,9 +141,6 @@ control 'os-05b' do
   impact 1.0
   title 'Check login.defs - RedHat specific'
   desc 'Check owner and permissions for login.defs. Also check the configured PATH variable and umask in login.defs'
-  describe file('/etc/login.defs') do
-    it { should_not be_writable }
-  end
   describe login_defs do
     its('SYS_UID_MIN') { should eq '201' }
     its('SYS_UID_MAX') { should eq '999' }
